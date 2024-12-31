fd2052a8285fe0fcc5e9123266a860d371674763
Julkar Naim <julkar.naim@abbvie.com>
2024-12-31 20:09:46 +0600

    test for util package

--- util/random_test.go
+++ util/random_test.go
@@ -1,1 +1,26 @@
 package util
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestRandomInt(t *testing.T) {
+	n := RandomInt(1000)
+	require.True(t, n <= 1000)
+}
+
+func TestRandomString(t *testing.T) {
+	s := RandomString(5)
+	require.True(t, len(s) <= 5)
+}
+
+func TestAccount(t *testing.T) {
+	owner := RandomOwner()
+	money := RandomMoney()
+	currency := RandomCurrency()
+	require.NotEmpty(t, owner)
+	require.NotZero(t, money)
+	require.NotEmpty(t, currency)
+	// test
+}
